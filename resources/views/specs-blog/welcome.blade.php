d7f8e924ccda85418d28bdaf97236d92a86796e7
specs lanre <specs.lanre@yandex.com>
2021-12-20 16:22:04 +0000

    added new blog blade view
    new blog blade view

---
+++ resources/views/specs-blog/welcome.blade.php
@@ -0,0 +1,87 @@
+<!Doctype html >
+<html>
+<head>
+<title>
+Specs Blog
+</title>
+<meta name="charset" content="utf-8">
+<meta name="viewport" content="width='device-width',
+initial-scale=1.0">
+
+<link rel="stylesheet" 
+href="{{ asset('css/responsive-navmenu.css')}}">
+<!--add font awesome cdn here  -->
+<link rel="stylesheet" 
+href="{{ asset('fontawesome-free-5-15-4-web/css/all.css') }}">
+</head>
+
+<body>
+<!--this is the main row-->
+<section  class="nav">
+<!--this is the left item column -->
+<nav class="">
+<label for="mchk">
+<span class="nav-item-menubar">
+<i class="fa fa-bars"></i>
+</span>
+</label>
+<input type="checkbox" id="mchk"
+class="mchkinput"
+>
+<span class="logo">
+NavMenu Logo
+</span>
+
+
+<!--this is the nav item containers -->
+
+<span class="nav-item-case">
+
+
+<span class="nav-item-link">
+<a href="menu.html">
+<i class="fa fa-home"></i>Home
+</a>
+</span>
+<span class="nav-item-link">
+<a href="topics.html">
+<i class="fa fa-th-list"></i>Topics
+</a>
+</span>
+
+
+<span class="nav-item-link">
+<a href="login.html">
+<i class="fas fa-sign-out-alt"></i>
+
+Login
+</a>
+</span>
+
+<span class="nav-item-link">
+<a href="signup.html">
+<i class="fas fa-user-plus"></i>
+Sign up
+</a>
+</span>
+
+<span class="nav-item-link">
+<a href="logout.html">
+<i class="fas fa-sign-out-alt"></i>
+Log out
+</a>
+</span>
+
+
+
+
+</span>
+
+
+<h4>Signup page </h4>
+
+
+</section>
+
+</body>
+</html>
